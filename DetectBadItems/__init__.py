08438d965f3a72eccd5284242daf0c4849986dd1
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-04-27 17:17:19 -0500

    a

--- DetectBadItems/__init__.py
+++ DetectBadItems/__init__.py
@@ -6,4 +6,4 @@ import azure.functions as func
 
 def main(servicebus: func.ServiceBusMessage, inputblob: func.InputStream):
     logging.info('Python DetectBadItems function started processing a message service bus: %s', servicebus)
-    logging.info('Python DetectBadItems function started processing a blob file: %s', inputblob.name)    
+    logging.info('Python DetectBadItems function started processing a blob file: %s', inputblob)    
